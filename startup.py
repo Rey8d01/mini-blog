1f2da8448d361bf0979f95d02aa2752dcbe64ca8
rey.8d01 <rey.8d01@yandex.ru>
2021-10-11 15:00:18 +0500

    Add fake data

--- startup.py
+++ startup.py
@@ -1,8 +1,22 @@
 """Инструкции для первого запуска приложения."""
 
+from faker import Faker
+
 from core.models import *
+from core.use_cases import create_new_post
 from main import create_app
 
 if __name__ == "__main__":
     # Настройка схемы БД.
-    db.create_all(app=create_app())
+    app = create_app()
+    app.app_context().push()
+    db.create_all(app=app)
+
+    # Заполнение фейковыми данными.
+    fake = Faker()
+    for _ in range(100):
+        create_new_post(
+            alias=fake.domain_word(),
+            title=fake.name(),
+            text=fake.text(),
+        )
